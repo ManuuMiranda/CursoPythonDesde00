6c300bd6ea5aa6426db56d3e703a508d4ae415cc
ManuMiranda <9001137@alumnos.ufv.es>
2022-03-09 12:30:44 +0100

    Bucles I Empezando

---
+++ Bucles I.py
@@ -0,0 +1,12 @@
+# Pueden ser determinados (se sabe número de veces a ejecutar) o indeterminados (no se sabe o dependerá o infinito):
+
+for i in [1, 2, 3]:
+    print("Hola")
+
+for i in ["primavera", "verano", "otoño", "invvierno"]:
+    print("Hola")
+
+for i in ["primavera", "verano", "otoño", "invvierno"]: # La variable se puede llamar i o como quieras
+    print(i)
+
+
