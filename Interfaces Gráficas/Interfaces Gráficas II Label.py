ecb08b4b4ae686ae5d726fdb66d3eea697bfe600
ManuMiranda <9001137@alumnos.ufv.es>
2022-08-18 14:23:02 +0200

    Interfaces Gráficas III LABEL

---
+++ Interfaces Gráficas/Interfaces Gráficas II Label.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+root = Tk()
+
+frame = Frame(root, width=500, height=400)
+
+frame.pack()
+
+label = Label(frame, text="TusMuertos")
+label.place(x=50, y=150)
+
+imagen = PhotoImage(file ="Label Parametros Tipicos.png")
+Label(frame, image=imagen).place(x=0, y=50)
+
+""" También se puede hacer sin poner variables: """
+
+Label(frame, text="TusMuertos", fg="blue", font=("Comic Sans MS", 18)).place(x=75, y=175)
+
+root.mainloop()
